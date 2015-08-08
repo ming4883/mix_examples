082ce1e5dc142cecd89250f70361539fc4a21fbc
Ka-ming Chan <ming4883@gmail.com>
2015-08-08 12:35:05 +0800

    added dynamic sized triangle rendering to example_01

--- src/example_01/app.cpp
+++ src/example_01/app.cpp
@@ -14,6 +14,8 @@ namespace example
 
             bgfx::DynamicVertexBufferHandle vtxbuf;
             bgfx::DynamicIndexBufferHandle idxbuf;
+            uint16_t idxmem[3];
+            float vtxmem[5*3];
 
             Triangle()
                 : vtxbuf (BGFX_INVALID_HANDLE)
@@ -35,6 +37,23 @@ namespace example
                 idxbuf = bgfx::createDynamicIndexBuffer (3, BGFX_BUFFER_NONE);
             }
 
+            void update (float _t)
+            {
+                float _s = cosf (_t) * 0.5f + 0.5f;
+                _s = 0.25f + 0.75f * _s;
+
+                idxmem[0] = 0u;
+                idxmem[1] = 1u;
+                idxmem[2] = 2u;
+
+                vtxmem[ 0] = 0.0f; vtxmem[ 1] = _s; vtxmem[ 2] = 0.5f; vtxmem[ 3] = 0.5f; vtxmem[ 4] = 1.0f;
+                vtxmem[ 5] =-_s; vtxmem[ 6] =-_s; vtxmem[ 7] = 0.5f; vtxmem[ 8] = 0.0f; vtxmem[ 9] = 0.0f;
+                vtxmem[10] = _s; vtxmem[11] =-_s; vtxmem[12] = 0.5f; vtxmem[13] = 1.0f; vtxmem[14] = 0.0f;
+
+                bgfx::updateDynamicVertexBuffer (vtxbuf, 0, bgfx::makeRef (vtxmem, sizeof (vtxmem)));
+                bgfx::updateDynamicIndexBuffer  (idxbuf, 0, bgfx::makeRef (idxmem, sizeof (idxmem)));
+            }
+
             void shutdown()
             {
                 bgfx::destroyDynamicVertexBuffer (vtxbuf);
@@ -49,31 +68,31 @@ namespace example
         {
             mix::Buffer _buf;
 
-            bgfx::ShaderHandle _vs, _fs;
+            bgfx::ShaderHandle _vsh, _fsh;
 
             {
                 mix::Result _ret = loadAsset (_buf, _vsPath);
                 if (_ret.isFail())
                 {
-                    mix::Log::e (getAppId(), "failed to load vs: %s", _ret.why());
+                    mix::Log::e (getAppId(), "failed to load vertex shader: %s", _ret.why());
                     return BGFX_INVALID_HANDLE;
                 }
 
-                _vs = bgfx::createShader (bgfx::copy (_buf.ptr(), _buf.size()));
+                _vsh = bgfx::createShader (bgfx::copy (_buf.ptr(), _buf.size()));
             }
 
             {
-                mix::Result _ret = loadAsset (_buf, _vsPath);
+                mix::Result _ret = loadAsset (_buf, _fsPath);
                 if (_ret.isFail())
                 {
-                    mix::Log::e (getAppId(), "failed to load vs: %s", _ret.why());
+                    mix::Log::e (getAppId(), "failed to load fragment shader: %s", _ret.why());
                     return BGFX_INVALID_HANDLE;
                 }
 
-                _fs = bgfx::createShader (bgfx::copy (_buf.ptr(), _buf.size()));
+                _fsh = bgfx::createShader (bgfx::copy (_buf.ptr(), _buf.size()));
             }
             
-            return bgfx::createProgram (_vs, _fs, true);
+            return bgfx::createProgram (_vsh, _fsh, true);
         }
         
 
@@ -113,7 +132,7 @@ namespace example
             bgfx::setViewRect (0, 0, 0, getMainFrontendDesc().width, getMainFrontendDesc().height);
             bgfx::touch (0);
 
-            float t = floorf (fmodf (getTimeSource().totalTimeInMS() * 0.0625f, 128.0f));
+            float t = floorf (fmodf (getTimeSource().totalTimeInMS() * (1 / 128.0f), 128.0f));
             
             bgfx::setViewClear (0
                 , BGFX_CLEAR_COLOR|BGFX_CLEAR_DEPTH
@@ -124,8 +143,15 @@ namespace example
 
             bgfx::dbgTextClear();
             bgfx::dbgTextPrintf (0, 1, 0x4f, "t = %.2f", t);
+
+            tri.update (t);
+
+            bgfx::setVertexBuffer (tri.vtxbuf);
+            bgfx::setIndexBuffer (tri.idxbuf);
+
+            bgfx::setState (BGFX_STATE_DEFAULT);
             
-            //bgfx::submit (0);
+            bgfx::submit (0, triProg);
             bgfx::frame ();
         }
 
